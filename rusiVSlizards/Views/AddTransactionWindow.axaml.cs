9a731475419430ad80137da3ef9efe9d131785cf
repack by iluhich <iluhadolmatov@bk.ru>
2023-11-17 04:36:26 +0300

    omg1

--- rusiVSlizards/Views/AddTransactionWindow.axaml.cs
+++ rusiVSlizards/Views/AddTransactionWindow.axaml.cs
@@ -1,4 +1,9 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using HarfBuzzSharp;
+using rusiVSlizards.Models;
+using rusiVSlizards.ViewModels;
+using System;
 
 namespace rusiVSlizards.Views
 {
@@ -7,6 +12,42 @@ namespace rusiVSlizards.Views
         public AddTransactionWindow()
         {
             InitializeComponent();
+
+            addButton.Click += delegate { AddTr(); };
+            cancelButton.Click += delegate { Close(null); };
+        }
+
+
+        private void AddTr()
+        {
+            Transaction tr = GetData();
+
+            if (tr == null)
+            {
+                return;
+            }
+
+            Db.AddTransaction(tr);
+            Close(null);
+        }
+
+        private Transaction GetData()
+        {
+            Transaction tr = new Transaction();
+
+            int amount;
+
+            if (userbox.SelectedIndex == -1 || transtypebox.SelectedIndex == -1 || !Int32.TryParse(amountText.Text, out amount))
+            {
+                return null;
+            }
+
+            tr.user = userbox.SelectedItem as Client;
+            tr.amount = amount;
+            tr.date = DateTime.Now;
+            tr.decs = descText.Text == string.Empty ? " " : descText.Text;
+            tr.transactionType = (TransactionType)transtypebox.SelectedItem;
+            return tr;
         }
     }
 }
